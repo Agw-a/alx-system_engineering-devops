9cf86a296dd543917c59286315fc12901ad4d557
Agw-a <lizagwa7@gmail.com>
2022-11-10 00:24:19 -0800

    all files --1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# increase number of failed requests
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
+}
